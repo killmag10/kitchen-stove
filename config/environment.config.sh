6983443b40cf114745e842cbfaafce8162ebfc30
Lars Dietrich <lars@dietrich-hosting.de>
2020-09-04 11:07:21 +0200

    [improvement] Add comment for GTK Theme option.

--- config/environment.config.sh
+++ config/environment.config.sh
@@ -4,3 +4,6 @@ export ETL_KETTLE_REPOSITORY_USER='admin'
 export ETL_KETTLE_REPOSITORY_PASSWORD='password'
 export ETL_KETTLE_MEMORY_MAX='${kettle.memory.max}'
 export ETL_KETTLE_LOG_LEVEL='${kettle.log.level}'
+
+## Set other GTK theme
+# export GTK_THEME=Numix
